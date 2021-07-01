8d013bdd920d35844de16ac096715a2a149a0734
marianoor786 <85068648+marianoor786@users.noreply.github.com>
2021-07-01 13:37:03 +0600

    Add a html and css files for portfolio

---
+++ js/dist/main.dev.js
@@ -0,0 +1,44 @@
+"use strict";
+
+(function ($) {
+  "use strict"; // Navbar on scrolling
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('.navbar').fadeIn('slow').css('display', 'flex');
+    } else {
+      $('.navbar').fadeOut('slow').css('display', 'none');
+    }
+  }); // Smooth scrolling on the navbar links
+
+  $(".navbar-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(this.hash).offset().top - 45
+      }, 1500, 'easeInOutExpo');
+
+      if ($(this).parents('.navbar-nav').length) {
+        $('.navbar-nav .active').removeClass('active');
+        $(this).closest('a').addClass('active');
+      }
+    }
+  }); // Scroll to Bottom
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-to-bottom').fadeOut('slow');
+    } else {
+      $('.scroll-to-bottom').fadeIn('slow');
+    }
+  }); // Skills
+
+  $('.skill').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+})(jQuery);
+//# sourceMappingURL=main.dev.js.map
